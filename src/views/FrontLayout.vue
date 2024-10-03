397371af7af20873049abea0c1e266b6a2915226
chcarrie <ppinkywei@gmail.com>
2024-10-03 19:24:11 +0800

    update

--- src/views/FrontLayout.vue
+++ src/views/FrontLayout.vue
@@ -60,10 +60,7 @@ export default {
     font-size: 24px;
     color: #6c584c;
   }
-}
-
-@media screen and (max-width: 767.98px) {
-  .top {
+  @media screen and (max-width: 767.98px) {
     bottom: 15px;
     right: 15px;
     i {
